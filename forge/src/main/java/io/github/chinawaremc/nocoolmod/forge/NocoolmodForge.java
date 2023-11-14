e59e54f44b7ed2bc28b999100c6e3c4d539557bb
baka4n <qq474899581@gmail.com>
2023-11-15 00:37:06 +0800

    commit project

---
+++ forge/src/main/java/io/github/chinawaremc/nocoolmod/forge/NocoolmodForge.java
@@ -0,0 +1,11 @@
+package io.github.chinawaremc.nocoolmod.forge;
+
+import dev.architectury.platform.forge.EventBuses;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("nocoolmod")
+public class NocoolmodForge {
+    public NocoolmodForge() {
+
+    }
+}
---
+++ forge/src/main/java/io/github/chinawaremc/nocoolmod/mixin/forge/RecipeManagerMixin.java
@@ -0,0 +1,29 @@
+package io.github.chinawaremc.nocoolmod.mixin.forge;
+
+import com.google.gson.JsonElement;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.util.profiling.ProfilerFiller;
+import net.minecraft.world.item.crafting.RecipeManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Map;
+
+@Mixin(RecipeManager.class)
+public class RecipeManagerMixin {
+    @Inject(
+            method = "apply(Ljava/util/Map;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)V",
+            at = @At("HEAD")
+    )
+    private void apply(Map<ResourceLocation, JsonElement> object,
+                       ResourceManager resourceManager,
+                       ProfilerFiller profiler,
+                       CallbackInfo ci) {
+        object.forEach((resourceLocation, jsonElement) -> {
+            System.out.println(resourceLocation);
+        });
+    }
+}
